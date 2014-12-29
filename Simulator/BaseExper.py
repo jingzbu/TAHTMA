381f42147f20841f233699cc1610e4b0095f76f0
jingzbu <jingzbu@gmail.com>
2014-12-29 12:06:20 -0500

    clean the mess

--- Simulator/BaseExper.py
+++ Simulator/BaseExper.py
@@ -1,1 +1,40 @@
-__author__ = 'jzh'
+import argparse
+import os
+from TAHTMA.util.util import abstract_method, load_para
+
+ROOT = os.environ.get('SADIT_ROOT')
+
+
+class BaseExper(object):
+    ROOT = ROOT
+
+    def __init__(self, argv, parser=None):
+        # import ipdb;ipdb.set_trace()
+        if parser is None:
+            parser = argparse.ArgumentParser(add_help=False)
+            # parser = argparse.ArgumentParser()
+        self.parser = parser
+        self.init_parser(parser)
+        self.args, self.res_args = parser.parse_known_args(argv)
+
+        if self.args.help:
+            self.print_help()
+            import sys; sys.exit(0)
+
+        if self.args.config is None:
+            print('You must specifiy --config option, run with -h option to see help')
+            import sys; sys.exit(0)
+
+    def print_help(self):
+        self.parser.print_help()
+
+    def init_parser(self, parser):
+        parser.add_argument('-h', '--help', default=False, action='store_true',
+                help="""print help message""")
+        parser.add_argument('-c', '--config', default=None,
+                # type=lambda x: load_para(x, Namespace),
+                type=lambda x: load_para(x),
+                help="""config""")
+
+    def run(self):
+        abstract_method()
--- Simulator/ThresCalc.py
+++ Simulator/ThresCalc.py
@@ -1,1 +1,61 @@
-__author__ = 'jzh'
+#!/usr/bin/env python
+"""
+This file contains all the threshold calculation methods
+"""
+from __future__ import absolute_import, division
+
+__author__ = "Jing Zhang"
+__email__ = "jingzbu@gmail.com"
+__status__ = "Development"
+
+import numpy as np
+from numpy import linalg as LA
+from math import log
+from matplotlib.mlab import prctile
+
+from TAHTMA.util.util import mu_ini, probability_matrix_Q, P_est, chain, mu_est, Q_est, G_est
+from TAHTMA.util.util import H_est, Sigma_est, W_est, KL_est, HoeffdingRuleMarkov
+
+
+class ThresBase(object):
+    def __init__(self, N, beta, n, mu_0, mu, mu_1, P, G_1, H_1, W_1):
+        self.N = N  # N is the row dimension of the original transition matrix Q
+        self.beta = beta  # beta is the false alarm rate
+        self.n = n  # n is the number of samples
+        self.mu_0 = mu_0
+        self.mu = mu
+        self.mu_1 = mu_1
+        self.P = P
+        self.G_1 = G_1
+        self.H_1 = H_1
+        self.W_1 = W_1
+
+class ThresActual(ThresBase):
+    """ Computing the actual (theoretical) threshold
+    """
+    def ThresCal(self):
+        SampNum = 1000
+        KL = []
+        for i in range(0, SampNum):
+            x = chain(self.mu_0, self.P, self.n)
+            mu = np.reshape(self.mu, (self.N, self.N))
+            KL.append(KL_est(x, mu))  # Get the actual relative entropy (K-L divergence)
+        self.eta = prctile(KL, 100 * (1 - self.beta))
+        eta = self.eta
+        return eta
+
+class ThresWeakConv(ThresBase):
+    """ Estimating the threshold by use of weak convergence
+    """
+    def ThresCal(self):
+        self.eta = HoeffdingRuleMarkov(self.beta, self.G_1, self.H_1, self.W_1, self.n)
+        eta = self.eta
+        return eta
+
+class ThresSanov(ThresBase):
+    """ Estimating the threshold by use of Sanov's theorem
+    """
+    def ThresCal(self):
+        self.eta = - log(self.beta) / self.n
+        eta = self.eta
+        return eta
